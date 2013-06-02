ce01aec86bbaf4936f9a57d2ff09182985b64b24
Alan Hawse <arh@elkhorn-creek.org>
2013-06-02 14:42:42 -0400

    updated chart

--- getCreek/src/makeChart.java
+++ getCreek/src/makeChart.java
@@ -74,7 +74,7 @@ static XYDataset readData(Integer amount,String sdate) {
 
 		//			String sql = "SELECT ts,depth,60min FROM ddata where ts>\"" + sdate + "\" order by ts limit " + amount +";";
 		amount = 8 * 60;
-		String sql = "SELECT ts,depth FROM ddata order by ts desc limit "+ amount + ";" ;
+		String sql = "SELECT ts,filter FROM ddata order by ts desc limit "+ amount + ";" ;
 
 		data.executeQuery(sql);
 
